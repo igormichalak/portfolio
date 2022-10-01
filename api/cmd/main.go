a960a9276cc7211e8808dc06f9468fe2790b5a53
Igor Michalak <git@igormichalak.com>
2022-10-01 23:46:39 +0200

    feat(api): establishing connection to the database

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type config struct {
 	port uint
+	dsn  string
 }
 
 type application struct {
@@ -20,11 +24,18 @@ func main() {
 	var cfg config
 
 	flag.UintVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.dsn, "dsn", "postgres://web:1234@db:5432/portfolio", "PostgreSQL data source name")
 
 	flag.Parse()
 
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	dbpool, err := openDB(cfg.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer dbpool.Close()
 
 	app := &application{
 		config:   cfg,
@@ -32,6 +43,17 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	err := app.serve()
+	err = app.serve()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	dbpool, err := pgxpool.New(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = dbpool.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+	return dbpool, nil
 }
