50a235386ac1983a2a190cde7610613adea5b0fa
Igor Michalak <git@igormichalak.com>
2022-10-13 19:38:52 +0200

    feat: update first db migration

--- api/migrations/001_init_database.sql
+++ api/migrations/001_init_database.sql
@@ -1,7 +1,25 @@
 CREATE TABLE blog_posts (
-    id integer PRIMARY KEY
+    id serial PRIMARY KEY,
+    slug varchar(96) UNIQUE NOT NULL,
+    title text NOT NULL,
+    body bytea NOT NULL,
+    created timestamp(0) with time zone NOT NULL,
+    updated timestamp(0) with time zone NOT NULL
+);
+
+CREATE TABLE blog_tags (
+    id serial PRIMARY KEY,
+    name varchar(24) UNIQUE NOT NULL
+);
+
+CREATE TABLE post_tags (
+  post_id int NOT NULL REFERENCES blog_posts(id) ON DELETE CASCADE,
+  tag_id int NOT NULL REFERENCES blog_tags(id) ON DELETE CASCADE,
+  PRIMARY KEY (post_id, tag_id)
 );
 
 ---- create above / drop below ----
 
-DROP TABLE blog_posts;
+DROP TABLE IF EXISTS blog_posts;
+DROP TABLE IF EXISTS blog_tags;
+DROP TABLE IF EXISTS post_tags;
