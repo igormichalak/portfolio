305f41ad5e7997658e0241055147f2bc95522329
Igor Michalak <git@igormichalak.com>
2022-11-08 21:36:10 +0100

    feat: update page layout

--- frontend/src/components/organisms/SideBar/SideBar.styles.ts
+++ frontend/src/components/organisms/SideBar/SideBar.styles.ts
@@ -1,3 +1,6 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.aside`
+  position: absolute;
+  inset: 0 auto 0 0;
+`;
--- frontend/src/components/templates/Layout/Layout.styles.ts
+++ frontend/src/components/templates/Layout/Layout.styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  width: 100vw;
   display: flex;
+  justify-content: center;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.main`
+  width: 768px;
+`;
